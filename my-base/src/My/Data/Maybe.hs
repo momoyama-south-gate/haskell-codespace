c961a5051fa2d95683486e470b7eaba6b750a0db
jietao_wu@wamazing.jp <jietao_wu@wamazing.jp>
2020-06-20 15:03:36 +0900

    finish maybe

--- my-base/src/My/Data/Maybe.hs
+++ my-base/src/My/Data/Maybe.hs
@@ -21,40 +21,82 @@ import My.Prelude.Internal
 
 -- data Maybe a = Nothing | Just a
 
-maybe :: b -> (a -> b) -> Maybe a -> b
 maybe b f ma =
   case ma of
     Just a -> f a
-    Nothing -> Nothing
+    Nothing -> b
 
 isJust :: Maybe a -> Bool
-isJust = undefined
+isJust ma =
+  case ma of
+    Just a -> True
+    Nothing -> False
 
 isNothing :: Maybe a -> Bool
-isNothing = undefined
+isNothing ma =
+  case ma of
+    Just a -> False
+    Nothing -> True
 
 fromMaybe :: a -> Maybe a -> a
-fromMaybe = undefined
+fromMaybe a1 ma =
+  case ma of
+    Just a -> a
+    _ -> a1
 
 listToMaybe :: [a] -> Maybe a
-listToMaybe = undefined
+listToMaybe l =
+  case l of
+    [] -> Nothing
+    a : tl -> Just a
 
 maybeToList :: Maybe a -> [a]
-maybeToList = undefined
+maybeToList ma =
+  case ma of
+    Just a -> [a]
+    Nothing -> []
 
 catMaybes :: [Maybe a] -> [a]
-catMaybes = undefined
+catMaybes l =
+  case l of
+    [] -> []
+    ma : tl ->
+      case ma of
+        Just a -> a : (catMaybes tl)
+        Nothing -> (catMaybes tl)
+
 
 mapMaybe :: (a -> Maybe b) -> [a] -> [b]
-mapMaybe = undefined
+mapMaybe f l =
+  case l of
+    [] -> []
+    a : tl ->
+      case (f a) of
+        Just b -> b : (mapMaybe f tl)
+        Nothing -> mapMaybe f tl
 
-instance Semigroup a => Semigroup (Maybe a)
+instance Semigroup a => Semigroup (Maybe a) where
+  Just a <> Nothing = Just a
+  Just a <> Just b = Just (a <> b)
+  Nothing <> Just b = Just b
+  Nothing <> Nothing = Nothing
 
-instance Semigroup a => Monoid (Maybe a)
+instance Semigroup a => Monoid (Maybe a) where
+  mempty = Nothing
 
-instance Functor Maybe
+instance Functor Maybe where
+  fmap f ma =
+    case ma of
+      Just a -> Just (f a)
+      Nothing -> Nothing
 
-instance Applicative Maybe
+instance Applicative Maybe where
+  pure = Just
+  (<*>) mf ma =
+    case (mf,ma) of
+      (Just f, Just a) -> Just (f a)
+      _ -> Nothing
+-- <*>::f(a -> b) -> f a -> f b
 
 instance Alternative Maybe
 
